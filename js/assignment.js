05c460b4393764a20a4935d6bd9ebe84c093fcba
Roman Krejcik <farin@farin.cz>
2016-12-18 14:08:03 +0100

    initial commit

---
+++ js/assignment.js
@@ -0,0 +1,21 @@
+
+console.log('%cTic Tac Toe ', 'color: #993300; font-size: 140%');
+console.log(`Naprogramuj klasické piškvorky 3x3
+
+Co by měla správná implementace umět:
+
+%c*%c klik na neobsazené políčko umístí symbol
+%c*%c hráči se střídají, pod nadpisem je zobrazeno kdo je na tahu
+%c*%c pokud jeden hráč zvítězí, vypsat vítěze a zamezit dalšímui pokládání
+%c*%c pokud si hodně rychlý přidej tlačítko pro reset hry
+
+Ve stylech jsou pripravene tridy %cx%c a %co%c.
+Hra pochopitelně začíná s přázdným plánem. Umístěné symboly jsou pouze ilustrační.
+`,
+'color: #993300', 'color: auto',
+'color: #993300', 'color: auto',
+'color: #993300', 'color: auto',
+'color: #993300', 'color: auto',
+'color: #993300', 'color: auto',
+'color: #993300', 'color: auto'
+);
